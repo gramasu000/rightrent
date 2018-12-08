3940fec2b6d4fc3645958a776fa39376c5722c05
Gautam <gramasu000@citymail.cuny.edu>
2018-12-08 14:05:59 -0500

    __init__.py comments

---
+++ rightrent/__init__.py
@@ -0,0 +1,21 @@
+# Imports
+
+
+"""
+In this file, the application factory function
+    accomplishes the following tasks
+----------------------------------------------------------
+    - Make a Flask instance that is set up to search instance folder
+            for database, and has a default configuration, a file configuration and testing configuration
+        (Copy from blog project)
+    - Check to see if instance folder exists
+    - Register the database
+    - Register the authentication blueprint
+    - Register the building blueprint
+    - Register the apartments blueprint
+    - Register the renter blueprint
+    - Register the payments blueprint
+    - Register the user blueprint
+    - Set app.route / to redirect to auth.index 
+"""
+
