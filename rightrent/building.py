a2abb698bb315817ae97d1875f8a7f4df4beca9d
Gautam <gramasu000@citymail.cuny.edu>
2018-12-13 18:35:09 -0500

    Comments for building.py - building blueprint

---
+++ rightrent/building.py
@@ -0,0 +1,44 @@
+# Imports
+
+# Create a blueprint object with url prefix bd/
+
+"""
+Endpoint for bd/add -> add
+Add the login required decorator
+    - If request method is POST
+        - Gets the building name and building address
+            from the submitted form data
+        - Checks if building name is not empty or null
+        - Calls check_building_duplicate 
+            to check for buildings in database with duplicate name
+        - If no errors,  
+            flash a message 
+            saying that building creation successful
+        - If errors, flash an error message 
+    - If request method is GET
+        simply serve the page
+"""
+
+"""
+Endpoint for bd/<building-id>/info
+Add the login required decorator
+    - Calls get_building(building_id) to 
+        get building information in dict form
+    - Load dict into render_template and serve the page 
+"""  
+
+"""
+Endpoint for bd/<user-id>/<building-id>/edit
+Add the login required decorator
+    - If request method is POST
+        - Collect form data into dict
+        - Check if new building name is not null
+        - Check if new building name is not taken
+            by calling check_building_duplicate
+        - Call edit_building(user_id, building_id)
+        - flash message that edit building is successful 
+    - If request method is GET
+        - Calls get_building(building_id)
+            to get building info in dict form
+        - Loads dict into render_template and serve the page 
+"""
